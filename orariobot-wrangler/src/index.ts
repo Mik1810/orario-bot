58523cdd8c91548c673fe6a9e3807c265d5e55f5
Luca Macera <lucamacera02@gmail.com>
2023-11-15 11:08:15 +0100

    Mo funziona

--- orariobot-wrangler/src/index.ts
+++ orariobot-wrangler/src/index.ts
@@ -3,19 +3,17 @@ let is_active: boolean = false
 
 
 export interface Env {
-	BOT_TOKEN:string
+	TOKEN:string
 	CHAT_ID: string
 }
-
+let count = 0
 export default {
-
 	async scheduled(event:any, env:Env, ctx:ExecutionContext) {
-		ctx.waitUntil(replyWithText(env.BOT_TOKEN, "313592737", "Sgozzati"))
+		await replyWithText(env.TOKEN, "313592737", "Sgozzati")
+        // add logic with Schedule Table for all the lectures...
 	},
 
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-
-	
 		if(request.method === 'POST'){
 			const payload = await request.json() as any
 			if('message' in payload){
@@ -24,34 +22,10 @@ export default {
 				//await replyWithText(env.BOT_TOKEN, payload.message.chat.id, payload.message.text)
 			}
 		}
-		return new Response('Hello World!');
+		return new Response(`count ${count}`);
 	},
 };
-
-async function sendMessage(token:string, chatID:string, message:string) {
-    const response = await fetch('https://api.telegram.org/bot' + token + '/sendMessage', {
-        method: 'POST',
-        headers: {  'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            chat_id: chatID,
-            text: message.toString().substring(0, 4096)
-        })
-    })
-    return response
-}
-
 async function replyWithText(token:string,chatID:string,text:string):Promise<Response>{
     const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${text}`;
     return fetch(url)
-}
-
-async function sendMex(token:string,chatID:string,text:string):Promise<Response> {
-    if (!is_active) {
-        is_active = true;
-		await fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${"Starting response"}`);
-        setInterval(async () => {
-            await fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${text}`);
-        }, 5000);
-    }
-    return new Response("Timer running...")
 }
