4a772189aaa98aaa0146c33960db73a98c3d1546
Mik1810 <michael.piccirilli@student.univaq.it>
2023-11-15 12:05:57 +0100

    Now the bot works

--- orariobot-wrangler/src/index.ts
+++ orariobot-wrangler/src/index.ts
@@ -1,30 +1,32 @@
-let is_active: boolean = false
-
-
-
 export interface Env {
 	TOKEN:string
 	CHAT_ID: string
 }
-let count = 0
+
+let lessons = 	["08:30 - 11-30 Robotica \n11:30-12:30 Reti \n14:30-16:30 Compilatori", 
+				"Martedì",
+				"08:30 - 11:30 Reti \n11:30 - 13:30 AI \n14:30 - 17:30 Inglese"]
+
 export default {
 	async scheduled(event:any, env:Env, ctx:ExecutionContext) {
-		await replyWithText(env.TOKEN, "313592737", "Sgozzati")
-        // add logic with Schedule Table for all the lectures...
+		await replyWithText(env.TOKEN, env.CHAT_ID, lessons[new Date().getDay() - 1])
 	},
+
+
 
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		if(request.method === 'POST'){
 			const payload = await request.json() as any
 			if('message' in payload){
-				console.log(payload)
+				//console.log(env.CHAT_ID)
 				//await replyWithText(env.BOT_TOKEN, payload.message.chat.id, payload.message.text)
 				//await replyWithText(env.BOT_TOKEN, payload.message.chat.id, payload.message.text)
 			}
 		}
-		return new Response(`count ${count}`);
+		return new Response(`Running...`);
 	},
 };
+
 async function replyWithText(token:string,chatID:string,text:string):Promise<Response>{
     const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${text}`;
     return fetch(url)
--- orariobot-wrangler/src/timer.ts
+++ orariobot-wrangler/src/timer.ts
@@ -1,12 +1,1 @@
 
-let is_active: boolean = false
-
-export async function sendMex(token:string,chatID:string,text:string):Promise<Response> {
-    if (!is_active) {
-        is_active = true;
-        setInterval(async () => {
-            await fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${text}`);
-        }, 60000);
-    }
-    return new Response("Timer running...")
-}
